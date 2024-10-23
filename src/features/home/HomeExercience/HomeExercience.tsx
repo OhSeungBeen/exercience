338e652f4a797f629ab8ce0994aed1e5b606e725
OhSeungBeen <seungbeenoh@gmail.com>
2024-10-23 20:25:37 +0900

    design: remove home exercience bg-fixed

--- src/features/home/HomeExercience/HomeExercience.tsx
+++ src/features/home/HomeExercience/HomeExercience.tsx
@@ -36,7 +36,7 @@ export default function HomeExercience() {
   });
 
   return (
-    <div className="exercience-container relative flex justify-center bg-home-running bg-cover bg-fixed bg-center sm:ml-[calc(-50vw+50%)] sm:w-screen">
+    <div className="exercience-container relative flex justify-center bg-home-running bg-cover bg-center sm:ml-[calc(-50vw+50%)] sm:w-screen">
       <div className="absolute inset-0 bg-black opacity-50" />
       <div className="relative z-10 flex aspect-square w-full sm:h-[800px] sm:w-[1024px]">
         <div className="text1 absolute left-1/2 top-[calc(50%+50px)] -translate-x-1/2 -translate-y-1/2 opacity-0">
